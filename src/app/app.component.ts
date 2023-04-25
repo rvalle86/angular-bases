c9c724eece7f97a21d9979ecd84bcd1454f5a910
Robert Magdiel Valle <rvalle@bancatlan.hn>
2023-04-25 16:35:31 -0600

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '02-bases';
+  public title: string = 'Hola Mundo';
+  // public counter: number = 10;
+
+  // increaseBy(value: number): void {
+  //   this.counter += value;
+  // }
+
+  // decreaseBy(value: number): void {
+  //   this.counter += value;
+  // }
+
+  // resetCounter(): void {
+  //   this.counter = 10;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
